a819e2d622699c680ab38211dc7db47162937231
spiratil <125731633+spiratil@users.noreply.github.com>
2025-05-05 16:00:38 +0930

    Update to build for Github Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev');
-
-/** @type {import('@sveltejs/kit').Config} */
 const config = {
+	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		}),
-		paths: {
-			base: dev ? '' : process.env.BASE_PATH,
-		}
+		appDir: 'app', // Required as the default is _app
+		adapter: adapter()
 	}
 };
 
